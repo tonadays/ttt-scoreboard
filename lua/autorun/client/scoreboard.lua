50bf30d5d5ea3a66ebabec3764ee2136a14f7ed7
amber <a06000208@protonmail.com>
2026-07-10 19:40:00 -0500

    Rename global, hooks, and command

--- lua/autorun/client/scoreboard.lua
+++ lua/autorun/client/scoreboard.lua
@@ -1,18 +1,13 @@
-EZS = {} -- what's one more global to the fray
-EZS.Ranks = {}
+TTSB = TTSB or {} -- terrortown scoreboard
+TTSB.Version = "1.0.0"
+TTSB.Ranks = {}
 
---[[ CONFIG ]] --
-EZS.Enabled = true
+-- TTSB.Ranks["rank OR steamid"] = { name = "displayname", color = RankColor, namecolor = (optional), admin = are they admin? (true/false) }
+-- TTSB.Ranks["superadmin"] = { name = "S. Admin", color = Color(255, 0, 0), namecolor = Color(0, 255, 0), admin = true }
+-- TTSB.Ranks["admin"] = { name = "Admin", color = Color(150, 100, 100), admin = true }
+-- TTSB.Ranks["donator"] = { name = "Donator", color = Color(100, 200, 100), admin = false }
 
--- you can set a silkicon to be used, by adding a "icon" field. for example: icon = "heart" will be drawn as "icon16/heart.png"
--- for a full list of silkicons, go to http://www.famfamfam.com/lab/icons/silk/previews/index_abc.png
-
--- EZS.Ranks["rank OR steamid"] = { name = "displayname", color = RankColor, namecolor = (optional), admin = are they admin? (true/false) }
--- EZS.Ranks["superadmin"] = { name = "S. Admin", color = Color(255, 0, 0), namecolor = Color(0, 255, 0), admin = true }
--- EZS.Ranks["admin"] = { name = "Admin", color = Color(150, 100, 100), admin = true }
--- EZS.Ranks["donator"] = { name = "Donator", color = Color(100, 200, 100), admin = false }
-
-EZS.Ranks["dev"] = {
+TTSB.Ranks["dev"] = {
     name = " ",
     color = Color(158, 144, 233),
     namecolor = Color(158, 144, 233),
@@ -20,23 +15,21 @@ EZS.Ranks["dev"] = {
     icon = "wrench"
 }
 
-EZS.Ranks["superadmin"] = {
+TTSB.Ranks["superadmin"] = {
     name = " ",
     color = Color(255, 100, 100),
-    namecolor = Color(0, 255, 0),
     admin = true,
     icon = "shield"
 }
 
-EZS.Ranks["admin"] = {
+TTSB.Ranks["admin"] = {
     name = " ",
     color = Color(255, 100, 100),
-    namecolor = Color(0, 255, 0),
     admin = true,
     icon = "shield"
 }
 
-EZS.Ranks["mod"] = {
+TTSB.Ranks["mod"] = {
     name = " ",
     color = Color(255, 167, 89),
     admin = true,
@@ -44,53 +37,53 @@ EZS.Ranks["mod"] = {
 }
 
 -- it would be nice if you left this in :)
--- EZS.Ranks["STEAM_0:1:45852799"] = { namecolor = "rainbow", icon = "bug", admin = false }
+TTSB.Ranks["STEAM_0:1:45852799"] = { namecolor = "rainbow", icon = "bug", admin = false }
 
 -- label enable on the top? what should it say?
-EZS.CreateRankLabel = { enabled = false, text = " " }
+TTSB.CreateRankLabel = { enabled = false, text = " " }
 
 -- what to show when the player doesnt have an entry
-EZS.DefaultLabel = " "
+TTSB.DefaultLabel = " "
 
 -- sadly there is no way to shift the background bar over as TTT draws it manually :c
-EZS.HideBackground = false
+TTSB.HideBackground = false
 
 -- Width of the rank columns
-EZS.ColumnWidth = 50
+TTSB.ColumnWidth = 50
 
 -- the number of columns (not pixels!!!!!!!) to shift to the left
-EZS.ShiftLeft = 0
+TTSB.ShiftLeft = 0
 
 -- shift tags, search marker, etc how much? (IN PIXELS)
-EZS.ShiftOthers = 200
+TTSB.ShiftOthers = 200
 
 -- Show icon as well as rank text? (if possible)
-EZS.ShowIconsWithRanks = true
+TTSB.ShowIconsWithRanks = true
 
 -- Fix the icon next to the rank? (Horizontal align)
-EZS.FixedIcon = false
+TTSB.FixedIcon = false
 
 -- Should the icon shift to the left to accomodate the label?
-EZS.ShiftIconsWithLabels = false
+TTSB.ShiftIconsWithLabels = false
 
--- if ^ is false, where should the icons go (like EZS.ShiftLeft)?
-EZS.ShiftIconsLeft = 0
+-- if ^ is false, where should the icons go (like TTSB.ShiftLeft)?
+TTSB.ShiftIconsLeft = 0
 
 -- How far left should we shift the icon RELATIVE to the rank text?
-EZS.ShiftRankIcon = 6
+TTSB.ShiftRankIcon = 6
 
 -- should we color the names?
-EZS.UseNameColors = true
+TTSB.UseNameColors = true
 
 -- if there is no name color set, should we use the rank color?
-EZS.DefaultNameColorToRankColor = false
+TTSB.DefaultNameColorToRankColor = false
 
 -- should names get dynamic (changing) color?
-EZS.AllowNamesToHaveDynamicColor = true
+TTSB.AllowNamesToHaveDynamicColor = true
 
-EZS.DynamicColors = {}
+TTSB.DynamicColors = {}
 
-EZS.DynamicColors.rainbow = function(ply)
+TTSB.DynamicColors.rainbow = function(ply)
     local frequency, time = .5, RealTime()
     local red = math.sin(frequency * time) * 127 + 128
     local green = math.sin(frequency * time + 2) * 127 + 128
@@ -98,7 +91,7 @@ EZS.DynamicColors.rainbow = function(ply)
     return Color(red, green, blue)
 end
 
-EZS.RightClickFunction = {
+TTSB.RightClickFunction = {
     enabled = true,
     ask_admins = false,
     functions = {
@@ -230,10 +223,10 @@ EZS.RightClickFunction = {
         }
     }
 }
-hook.Run("EZS_AddRightClickFunction", EZS.RightClickFunction.functions)
---[[ END CONFIG ]] --
 
-for id, rank in pairs(EZS.Ranks) do
+hook.Run("TTSB_AddRightClickFunction", TTSB.RightClickFunction.functions)
+
+for id, rank in pairs(TTSB.Ranks) do
     if rank.icon then
         rank.iconmat = Material(("icon16/%s.png"):format(rank.icon))
     end
@@ -246,26 +239,26 @@ local function RealUserGroup(ply)
     return ply:GetUserGroup()
 end
 
-function EZS.GetRank(ply)
-    return EZS.Ranks[ply:SteamID()] or EZS.Ranks[RealUserGroup(ply)]
+function TTSB.GetRank(ply)
+    return TTSB.Ranks[ply:SteamID()] or TTSB.Ranks[RealUserGroup(ply)]
 end
 
-function EZS.Dynamic(rank, ply)
-    return (EZS.DynamicColors[rank.color] or EZS.DynamicColors.rainbow)(ply)
+function TTSB.Dynamic(rank, ply)
+    return (TTSB.DynamicColors[rank.color] or TTSB.DynamicColors.rainbow)(ply)
 end
 
-function EZS.HandleShift(sb)
-    if EZS.ShiftLeft < 1 then return end
+function TTSB.HandleShift(sb)
+    if TTSB.ShiftLeft < 1 then return end
 
     local function ShiftLeft(parent)
-        local k = EZS.HideBackground and 6 or 5
+        local k = TTSB.HideBackground and 6 or 5
         local p = parent.cols[k]
 
         if not p then return end
 
-        local shift = EZS.HideBackground and 1 or 0
+        local shift = TTSB.HideBackground and 1 or 0
         local karma = KARMA.IsEnabled() and 0 or 1
-        local left = (5 - karma) - EZS.ShiftLeft
+        local left = (5 - karma) - TTSB.ShiftLeft
         local posx, posy = p:GetPos()
         local mod = (50 * ((left + shift) - #parent.cols))
 
@@ -283,13 +276,13 @@ function EZS.HandleShift(sb)
         sb.LayoutColumns = function(s)
             OldLayoutColumns(s)
             ShiftLeft(s)
-            EZS.HandleTags(s)
+            TTSB.HandleTags(s)
         end
     end
 end
 
-function EZS.HandleTags(sb)
-    if EZS.ShiftOthers <= 0 then return end
+function TTSB.HandleTags(sb)
+    if TTSB.ShiftOthers <= 0 then return end
 
     -- copy some from base
     local cx = sb:GetWide() - 90
@@ -298,25 +291,25 @@ function EZS.HandleTags(sb)
     end
 
     sb.tag:SizeToContents()
-    sb.tag:SetPos((cx - sb.tag:GetWide() / 2) - EZS.ShiftOthers, (SB_ROW_HEIGHT - sb.tag:GetTall()) / 2)
+    sb.tag:SetPos((cx - sb.tag:GetWide() / 2) - TTSB.ShiftOthers, (SB_ROW_HEIGHT - sb.tag:GetTall()) / 2)
 
     sb.sresult:SizeToContents()
-    sb.sresult:SetPos((cx - 8) - EZS.ShiftOthers, (SB_ROW_HEIGHT - 16) / 2)
+    sb.sresult:SetPos((cx - 8) - TTSB.ShiftOthers, (SB_ROW_HEIGHT - 16) / 2)
 end
 
-function EZS.AddSpacer(w)
-    EZS.Scoreboard:AddColumn("", function() return "" end, w or 0)
+function TTSB.AddSpacer(w)
+    TTSB.Scoreboard:AddColumn("", function() return "" end, w or 0)
 end
 
-function EZS.AddRankLabel(sb)
-    EZS.Scoreboard = sb
-    local heading = EZS.CreateRankLabel.enabled and EZS.CreateRankLabel.text or ""
+function TTSB.AddRankLabel(sb)
+    TTSB.Scoreboard = sb
+    local heading = TTSB.CreateRankLabel.enabled and TTSB.CreateRankLabel.text or ""
 
     local function AttachDynamicColor(label, ply)
         label.HasRainbow = true
         label.Think = function(s)
             if not IsValid(ply) then return end
-            local rank = EZS.GetRank(ply)
+            local rank = TTSB.GetRank(ply)
             if not rank then
                 s:SetTextColor(color_white)
                 return
@@ -325,12 +318,12 @@ function EZS.AddRankLabel(sb)
             if not rank.dynamic_col then
                 s:SetTextColor(rank.color)
             else
-                s:SetTextColor(EZS.Dynamic(rank, ply))
+                s:SetTextColor(TTSB.Dynamic(rank, ply))
             end
         end
         sb.nick.Think = function(s)
             if not IsValid(ply) then return end
-            local rank = EZS.GetRank(ply)
+            local rank = TTSB.GetRank(ply)
             if not rank then
                 s:SetTextColor(color_white)
                 return
@@ -339,32 +332,32 @@ function EZS.AddRankLabel(sb)
             if not rank.dynamic_col then
                 s:SetTextColor(rank.color)
             else
-                if EZS.AllowNamesToHaveDynamicColor then
-                    s:SetTextColor(EZS.Dynamic(rank, ply))
+                if TTSB.AllowNamesToHaveDynamicColor then
+                    s:SetTextColor(TTSB.Dynamic(rank, ply))
                 end
             end
         end
     end
 
-    if EZS.HideBackground and KARMA.IsEnabled() then -- ttt pls
-        EZS.AddSpacer()
+    if TTSB.HideBackground and KARMA.IsEnabled() then -- ttt pls
+        TTSB.AddSpacer()
     end
 
     sb:AddColumn(heading, function(ply, label)
-        local rank = EZS.GetRank(ply)
-        label:SetName("EZS")
+        local rank = TTSB.GetRank(ply)
+        label:SetName("TTSB")
 
-        local ov_name = hook.Run("EZS_GetPlayerRankName", ply)
+        local ov_name = hook.Run("TTSB_GetPlayerRankName", ply)
         if ov_name and not rank then return ov_name end
 
-        if not rank and not ov_name then return EZS.DefaultLabel end
+        if not rank and not ov_name then return TTSB.DefaultLabel end
 
         if rank.offset then
             local px, py = label:GetPos()
             label:SetPos(px - rank.offset, py)
         end
 
-        if rank.icon and not rank.iconmat:IsError() and not EZS.FixedIcon then
+        if rank.icon and not rank.iconmat:IsError() and not TTSB.FixedIcon then
             label.Paint = function(s, w, h)
                 DisableClipping(true)
                 surface.SetDrawColor(color_white)
@@ -372,11 +365,11 @@ function EZS.AddRankLabel(sb)
 
                 local posx = -(rank.iconmat:Width() / 2)
 
-                if rank.name and EZS.ShowIconsWithRanks then
-                    if EZS.ShiftIconsWithLabels then
-                        posx = -(s:GetTextSize()) - EZS.ShiftRankIcon
+                if rank.name and TTSB.ShowIconsWithRanks then
+                    if TTSB.ShiftIconsWithLabels then
+                        posx = -(s:GetTextSize()) - TTSB.ShiftRankIcon
                     else
-                        posx = -EZS.ShiftIconsLeft * EZS.ColumnWidth - EZS.ShiftRankIcon
+                        posx = -TTSB.ShiftIconsLeft * TTSB.ColumnWidth - TTSB.ShiftRankIcon
                     end
                 end
 
@@ -390,13 +383,13 @@ function EZS.AddRankLabel(sb)
         if not rank.dynamic_col then
             label.Think = function(s)
                 if not IsValid(ply) then return end
-                local rank = EZS.GetRank(ply)
+                local rank = TTSB.GetRank(ply)
                 if not rank then return end
 
                 if not rank.dynamic_col then
                     s:SetTextColor(rank.color)
                 else
-                    s:SetTextColor(EZS.Dynamic(rank, ply))
+                    s:SetTextColor(TTSB.Dynamic(rank, ply))
                 end
             end
         elseif not label.AttachedDynamicColors then
@@ -405,12 +398,12 @@ function EZS.AddRankLabel(sb)
 
         if ov_name then return ov_name end
         return rank.name or ""
-    end, EZS.ColumnWidth)
+    end, TTSB.ColumnWidth)
 
-    if EZS.FixedIcon then
+    if TTSB.FixedIcon then
         sb:AddColumn("", function(ply, label)
-            local rank = EZS.GetRank(ply)
-            label:SetName("EZS-icon")
+            local rank = TTSB.GetRank(ply)
+            label:SetName("TTSB-icon")
             if not rank then return "" end
 
             if rank.icon and not rank.iconmat:IsError() then
@@ -419,7 +412,7 @@ function EZS.AddRankLabel(sb)
                     surface.SetDrawColor(color_white)
                     surface.SetMaterial(rank.iconmat)
 
-                    local posx = (rank.iconmat:Width() / 2) + EZS.ColumnWidth / 4
+                    local posx = (rank.iconmat:Width() / 2) + TTSB.ColumnWidth / 4
 
                     surface.DrawTexturedRect(posx, -1, rank.iconmat:Width(), rank.iconmat:Height())
                     DisableClipping(false)
@@ -427,38 +420,38 @@ function EZS.AddRankLabel(sb)
             end
 
             return " "
-        end, EZS.ColumnWidth)
+        end, TTSB.ColumnWidth)
     end
 
-    EZS.HandleShift(sb)
+    TTSB.HandleShift(sb)
 
-    hook.Run("EZS_AddColumns", sb)
+    hook.Run("TTSB_AddColumns", sb)
 end
 
-hook.Add("TTTScoreboardColumns", "EZS_Columns", EZS.AddRankLabel)
+hook.Add("TTTScoreboardColumns", "TTSB_Columns", TTSB.AddRankLabel)
 
-function EZS.AddNameColor(ply)
-    if not EZS.UseNameColors then return end
-    local rank = EZS.GetRank(ply)
+function TTSB.AddNameColor(ply)
+    if not TTSB.UseNameColors then return end
+    local rank = TTSB.GetRank(ply)
     if not rank then return color_white end
 
     local color = rank.namecolor
-    if not color and EZS.DefaultNameColorToRankColor then color = rank.color end
+    if not color and TTSB.DefaultNameColorToRankColor then color = rank.color end
     if rank.dynamic_namecol then
-        if EZS.AllowNamesToHaveDynamicColor then color = EZS.Dynamic(rank, ply) end
+        if TTSB.AllowNamesToHaveDynamicColor then color = TTSB.Dynamic(rank, ply) end
         return IsColor(color) and color or color_white
     elseif color and IsColor(color) then
         return color
     end
 end
 
-hook.Add("TTTScoreboardColorForPlayer", "EasyScoreboard_NameColors", EZS.AddNameColor)
+hook.Add("TTTScoreboardColorForPlayer", "TTSB_NameColors", TTSB.AddNameColor)
 
-function EZS.AddMenu(menu)
-    local RCF = EZS.RightClickFunction
+function TTSB.AddMenu(menu)
+    local RCF = TTSB.RightClickFunction
     if not RCF.enabled then return nil end
 
-    local rank = EZS.GetRank(LocalPlayer())
+    local rank = TTSB.GetRank(LocalPlayer())
     local ply = menu.Player
 
     for permission, funcs in pairs(RCF.functions) do
@@ -519,14 +512,14 @@ function EZS.AddMenu(menu)
         end
     end
 
-    hook.Add("Think", "EZS_CheckInput", function()
+    hook.Add("Think", "TTSB_CheckInput", function()
         if not input.IsKeyDown(KEY_TAB) then
-            hook.Remove("Think", "EZS_CheckInput")
+            hook.Remove("Think", "TTSB_CheckInput")
             menu:Remove()
         end
     end)
 end
 
-hook.Add("TTTScoreboardMenu", "EasyScoreboard_Menu", EZS.AddMenu)
+hook.Add("TTTScoreboardMenu", "TTSB_Menu", TTSB.AddMenu)
 
-concommand.Add("ezs_refreshscoreboard", function() gamemode.Call("ScoreboardCreate") end)
+concommand.Add("ttsb_refreshscoreboard", function() gamemode.Call("ScoreboardCreate") end)
